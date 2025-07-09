1bfc6d52baef336c7d20cf3fd5e233f821e99aea
Rohith <93564193+codewithrohith-dev@users.noreply.github.com>
2025-07-09 22:02:24 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00FFFF',
+        secondary: '#00E096',
+        dark: '#0A0F1C',
+        card: '#0F172A',
+        muted: '#A1A1AA',
+        glowGreen: '#00FF94',
+        glowPink: '#FF6EC7',
+      },
+    },
   },
   plugins: [],
 }
-
